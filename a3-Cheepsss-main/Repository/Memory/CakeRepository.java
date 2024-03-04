da426bb7f94ce994cfd95b784d5a7d0888bcd826
Cheepsss <63982954+Cheepsss@users.noreply.github.com>
2024-03-04 22:54:24 +0200

    Delete a3-Cheepsss-main directory

--- a3-Cheepsss-main/Repository/Memory/CakeRepository.java
+++
@@ -1,6 +0,0 @@
-package Repository.Memory;
-
-import Domain.Cake;
-
-public class CakeRepository extends EntityRepository<Cake> {
-}
--- a3-Cheepsss-main/Repository/Memory/OrderRepository.java
+++
@@ -1,6 +0,0 @@
-package Repository.Memory;
-
-import Domain.Order;
-
-public class OrderRepository extends EntityRepository<Order> {
-}
